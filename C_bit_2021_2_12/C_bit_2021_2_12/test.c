a32e2e3eb1b89ce9e7b055a251c52f079e66c480
YJYCXD <824405484@qq.com>
2021-02-12 19:04:51 +0800

    2021-2-12

---
+++ C_bit_2021_2_12/C_bit_2021_2_12/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//KiKi想知道从键盘输入的两个数的大小关系，请编程实现。
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n1 = 0;
+//	int n2 = 0;
+//	while (~scanf("%d%d", &n1, &n2))
+//	{
+//		if (n1>n2)
+//			printf("%d>%d\n", n1, n2);
+//		else if (n1<n2)
+//			printf("%d<%d\n", n1, n2);
+//		else
+//			printf("%d=%d\n", n1, n2);
+//	}
+//	return 0;
+//}
+
+
+
+//现在试求单位冲激函数在时域t上的值。
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	double t = 0.0;
+//	while (~scanf("%lf", &t))
+//	{
+//		if (t>0)
+//			printf("1\n");
+//		else if (t<0)
+//			printf("0\n");
+//		else
+//			printf("0.5\n");
+//	}
+//	return 0;
+//}
+
+
